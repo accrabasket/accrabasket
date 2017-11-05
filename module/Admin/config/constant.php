a6bb4b1fb32a0d0212da4f437db2317b41fe3ade
Pramod2712 <pramodtiwari2712@gmail.com>
2017-11-05 18:58:49 +0530

    marchentview

--- module/Admin/config/constant.php
+++ module/Admin/config/constant.php
@@ -5,3 +5,4 @@ $GLOBALS['SITE_COMPANY_URL'] = 'http://' .$_SERVER['HTTP_HOST'].'/accrabasket/co
 $GLOBALS['PAGE_BEFORE_LOGIN'] = array('Admin\Controller\Index\login','Admin\Controller\Index\index');
 $GLOBALS['SITE_PATH'] = $_SERVER['DOCUMENT_ROOT'];
 define('NODE_API', 'http://localhost:3000/');
+define('BASKET_API', 'http://localhost/basketapi/');
