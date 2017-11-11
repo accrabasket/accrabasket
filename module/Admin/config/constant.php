66a0cde56d7659670f09b45af424bedb14de78df
pramodtiwari2712@gmail.com <you@example.com>
2017-11-11 17:34:23 +0530

    productlist

--- module/Admin/config/constant.php
+++ module/Admin/config/constant.php
@@ -1,10 +1,13 @@
 <?php
-$GLOBALS['HTTP_SITE_ADMIN_URL'] = 'http://' .$_SERVER['HTTP_HOST'].'/accrabasket/admin/';
-$GLOBALS['SITE_APP_URL'] = 'http://' .$_SERVER['HTTP_HOST'].'/accrabasket/application/index';
+$GLOBALS['HTTP_SITE_ADMIN_URL'] = 'http://' .$_SERVER['HTTP_HOST'].'/accrabasket/index.php/admin/';
+$GLOBALS['SITE_APP_URL'] = 'http://' .$_SERVER['HTTP_HOST'].'/accrabasket/index.php/application/index';
 $GLOBALS['SITE_COMPANY_URL'] = 'http://' .$_SERVER['HTTP_HOST'].'/accrabasket/company/';
 $GLOBALS['PAGE_BEFORE_LOGIN'] = array('Admin\Controller\Index\login','Admin\Controller\Index\index');
 $GLOBALS['SITE_PATH'] = $_SERVER['DOCUMENT_ROOT'];
 define('NODE_API', 'http://localhost:3000/');
-define('BASKET_API', 'http://localhost/basketapi/');
+define('BASKET_API', 'http://localhost/basketapi/index.php/');
 $GLOBALS['PRODUCTIMAGEPATH'] = $_SERVER['DOCUMENT_ROOT'].'accrabasket/product_img';
 $GLOBALS['ATTRIBUTEIMAGEPATH'] = $_SERVER['DOCUMENT_ROOT'].'accrabasket/attribute_img';
+$GLOBALS['HTTP_SITE_ADMIN_URL'] = 'http://' .$_SERVER['HTTP_HOST'].'/accrabasket/index.php/admin/';
+$GLOBALS['SITE_APP_URL'] = 'http://' .$_SERVER['HTTP_HOST'].'/accrabasket/index.php/application/index';
+define('BASKET_API', 'http://localhost/basketapi/index.php/');
