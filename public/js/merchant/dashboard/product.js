3fe202d6a6fa883634aa6c55204596cd8022e702
pramodtiwari2712@gmail.com <you@example.com>
2018-02-20 22:17:31 +0530

    search

--- public/js/merchant/dashboard/product.js
+++ public/js/merchant/dashboard/product.js
@@ -3,6 +3,44 @@ app.controller('productController', function ($scope, $http, $sce,$timeout) {
     $scope.currentPage = 1;
     $scope.filterProduct.page = 1;
     $scope.ajaxLoadingData = false;
+    
+    $scope.selected_filter_level = 'Action';
+    $scope.setFilterType = function(id){
+        $scope.filterProduct.filter_type = id;
+        $scope.selected_filter_level = id.replace("_", " ");
+    }
+    $scope.querySearch = function(){
+        if($scope.selected_filter_level == 'Action'){
+           $scope.errorStatus = true;
+           $scope.errorMsg = 'Please select a action '; 
+        }
+        
+        if($scope.filterText == '' || $scope.filterText == undefined){
+           $scope.errorStatus = true;
+           $scope.errorMsg = 'Please enter '+$scope.selected_filter_level; 
+        }
+        if(!$scope.errorStatus){
+            $scope.filterProduct.value = $scope.filterText;
+            delete $scope.filterProduct.page;
+            $scope.getProductList();
+        }else{
+            $timeout(function () {
+                $scope.errorStatus = false;
+                $scope.errorMsg = '';
+            }, 2000);
+        }
+        
+    }
+    
+    $scope.refresh = function() {
+        $scope.filterProduct = {};
+        $scope.filterProduct.page = 1;
+        $scope.selected_filter_level = 'Action';
+        $scope.filterText = '';
+        $scope.getProductList();
+    }
+    
+    
    $scope.getProductList = function() {
        $scope.ajaxLoadingData = true;
 //       $scope.filterProduct.pagination = 1;
