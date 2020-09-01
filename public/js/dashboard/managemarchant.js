98ccaf4fc6bcbbab66b94d820ee147fed7e71836
ravisingh2 <raviducat@gmail.com>
2020-09-01 22:38:31 +0530

    Update managemarchant.js

--- public/js/dashboard/managemarchant.js
+++ public/js/dashboard/managemarchant.js
@@ -60,3 +60,16 @@ app.controller('marchantController', function ($scope, $http, $sce,$timeout, mar
 	
 	
 });	
+
+ function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $('#merchant_img').val(e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+function uploadImageInTemp(obj) {
+    readURL(obj);
+}
