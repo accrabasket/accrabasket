9719d06fdedff65d2365184e72d3a294e6ca757c
root <root@ubuntu.members.linode.com>
2023-05-30 16:56:41 +0000

    last commit

--- module/Admin/config/constant.php
+++ module/Admin/config/constant.php
@@ -5,8 +5,8 @@ $GLOBALS['SITE_COMPANY_URL'] = 'http://' .$_SERVER['HTTP_HOST'].'/accrabasket/me
 $GLOBALS['HTTP_SITE_MERCHANT_URL'] = 'http://' .$_SERVER['HTTP_HOST'].'/accrabasket/merchant/';
 $GLOBALS['PAGE_BEFORE_LOGIN'] = array('Admin\Controller\Index\login','Admin\Controller\Index\index');
 $GLOBALS['SITE_PATH'] = $_SERVER['DOCUMENT_ROOT'];
-define('NODE_API', 'http://localhost:3000/');
-define('BASKET_API', 'http://localhost/basketapi/');
+define('NODE_API', 'http://172.104.239.54:3000/');
+define('BASKET_API', 'http://172.104.239.54/basketapi/');
 $GLOBALS['PRODUCTIMAGEPATH'] = $_SERVER['DOCUMENT_ROOT'].'accrabasket/product_img';
 $GLOBALS['ATTRIBUTEIMAGEPATH'] = $_SERVER['DOCUMENT_ROOT'].'accrabasket/attribute_img';
 define('APIKEY', 'secure#api$__');
